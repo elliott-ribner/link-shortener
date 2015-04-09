3377a6432ec6088f5269a91d8ebdc7534c32568c
Richard Davis <rdavis.bacs@gmail.com>
2015-04-09 13:59:37 -0400

    simplified schema

--- schema.sql
+++ schema.sql
@@ -1,57 +1,6 @@
---
--- PostgreSQL database dump
---
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SET check_function_bodies = false;
-SET client_min_messages = warning;
-
---
--- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
---
-
-CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
-
-
---
--- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
---
-
-COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
-
-
-SET search_path = public, pg_catalog;
-
-SET default_tablespace = '';
-
-SET default_with_oids = false;
-
---
--- Name: list_table; Type: TABLE; Schema: public; Owner: user; Tablespace: 
---
+DROP TABLE list_table;
 
 CREATE TABLE list_table (
     id character varying(100),
     url character varying(100)
 );
-
-
-ALTER TABLE list_table OWNER TO "user";
-
---
--- Name: public; Type: ACL; Schema: -; Owner: user
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM "user";
-GRANT ALL ON SCHEMA public TO "user";
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
-
---
--- PostgreSQL database dump complete
---
-
